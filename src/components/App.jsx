9641d5d5c09da76923a266bb3cea2e02eee89cf6
ashish6199 <47033568+ashish6199@users.noreply.github.com>
2021-06-06 23:12:18 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./header";
+import Note from "./note";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
